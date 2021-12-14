d01f37bf069599b8098e545481faa39c0e3859fd
HenryDodo <33288812+HenryDodo@users.noreply.github.com>
2021-12-14 14:03:00 +0800

    12/14 Version 1.1

--- public/JS/index.js
+++ public/JS/index.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
     $('#bg').fadeIn(1500)
     $('#login').fadeIn(2000)
     $("#submit").click(function () {
-        var checkAccount = $("#memberAccount").val();
-        var checkPassword = $("#memberPassword").val();
-        console.log(checkAccount);
-        console.log(checkPassword);
-        document.location.href = "/main/news";
+        var checkAccount = $('#memberAccount').val()
+        var checkPassword = $('#memberPassword').val()
+        const check = { checkAccount ,checkPassword}
+        document.location.href = "/main/news"
     });
 })
+
 
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express")
+const checkNum = require("./public/JS/index")
 const NetWork = express()
 const NetWorkRouter = require('./routes/NetWorkRouter')
 
-//const bcrypt = require("bcrypt")
-const saltRounds = 10; //整數型態，數值越高越安全
+const bcrypt = require("bcrypt")
+const saltRounds = 10 //整數型態，數值越高越安全
 
+const storePassword = ""
 
 NetWork.set("view engine", "pug")
 NetWork.get('/', (req, res) => {
@@ -13,55 +15,37 @@ NetWork.get('/', (req, res) => {
 });
 //test-----------------------------------------------------------
 NetWork.get('/main/news', (req, res) => {
-        // const myPassword = document.getElementById("memerAccount").val();
-        // bcrypt.hash(myPassword, saltRounds).then(function (hash) {
-        //     console.log(hash);
-        // });
+        var checkPassword = req.body.checkNum[1]
+        console.log(checkPassword)
+        bcrypt.hash(checkPassword, saltRounds).then(function (hash) {
+          console.log(hash);
+          storePassword = hash;
+        });
         console.log('導引至最新消息')
         res.render('main/news')
     })
-    //---------------------------------------------------------------
+//---------------------------------------------------------------
 NetWork.get('/main/edit_news', (req, res) => {
-        // const myPassword = document.getElementById("memerAccount").val();
-        // bcrypt.hash(myPassword, saltRounds).then(function (hash) {
-        //     console.log(hash);
-        // });
         console.log('導引至編輯消息')
         res.render('main/edit_news')
     })
-    //-----------------------------------------------------------------
+//-----------------------------------------------------------------
 NetWork.get('/main/handjob', (req, res) => {
-        // const myPassword = document.getElementById("memerAccount").val();
-        // bcrypt.hash(myPassword, saltRounds).then(function (hash) {
-        //     console.log(hash);
-        // });
         console.log('導引至手動排班')
         res.render('main/handjob')
     })
     //-----------------------------------------------------------------
 NetWork.get('/main/member', (req, res) => {
-        // const myPassword = document.getElementById("memerAccount").val();
-        // bcrypt.hash(myPassword, saltRounds).then(function (hash) {
-        //     console.log(hash);
-        // });
         console.log('導引至員工名單')
         res.render('main/member')
     })
     //-----------------------------------------------------------------
 NetWork.get('/main/memberRest', (req, res) => {
-        // const myPassword = document.getElementById("memerAccount").val();
-        // bcrypt.hash(myPassword, saltRounds).then(function (hash) {
-        //     console.log(hash);
-        // });
         console.log('導引至員工劃休')
         res.render('main/memberRest')
     })
     //-----------------------------------------------------------------
 NetWork.get('/main/schedule', (req, res) => {
-        // const myPassword = document.getElementById("memerAccount").val();
-        // bcrypt.hash(myPassword, saltRounds).then(function (hash) {
-        //     console.log(hash);
-        // });
         console.log('導引至排班表')
         res.render('main/schedule')
     })
